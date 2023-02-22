068ce23d782c67177da8069646120148dc316e0d
Diego <diegoyon@outlook.com>
2023-02-22 09:05:53 -0600

    add new note page

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -8,8 +8,22 @@ class NotesController < ApplicationController
   end
 
   def new
+    @note = Note.new
   end
 
   def create
+    @note = Note.new(note_params)
+
+    if @note.save
+      redirect_to @note
+    else
+      render :new, status: :unprocessable_entity 
+    end
   end
+
+  private
+    def note_params
+      params.require(:note).permit(:title, :body)
+    end
+
 end
