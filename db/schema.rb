d693379b712771f775a7e3e36adcbb927f8361b8
Diego <diegoyon@outlook.com>
2023-02-23 10:23:34 -0600

    add associations between topic and notes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_22_144357) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_23_162023) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_22_144357) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "topic_id", null: false
+    t.index ["topic_id"], name: "index_notes_on_topic_id"
   end
 
+  create_table "topics", force: :cascade do |t|
+    t.string "name"
+    t.string "icon"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "notes", "topics"
 end
